ade5dba25b52b03e4e68a5a5e0aba6d52c2078ac
kumara291098 <kumara291098@gmail.com>
2023-12-02 15:53:08 +0530

    Expance tracket is completed

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 const IncomeExpenses = () => {
+    const {transactions}=useContext(GlobalContext);
+    const amount=transactions.map(transation=>transation.amount);
+    const income=amount.filter(item=>item>0).reduce((acc, item)=>(acc+=item),0).toFixed(2);
+    const expenses=amount.filter(item=>item<0).reduce((acc, item)=>(acc+=item),0)*(-1).toFixed(2);
+    console.log(income)
     return (
         <div className="inc-exp-container">
             <div>
                 <h4>Income</h4>
-                <p id="money-plus" className="money plus">+&#x20B9;0.00</p>
+                <p id="money-plus" className="money plus">&#x20B9;{income} </p>
             </div>
             <div>
                 <h4>Expense</h4>
-                <p id="money-minus" className="money minus">-&#x20B9;0.00</p>
+                <p id="money-minus" className="money minus">&#x20B9; {expenses} </p>
             </div>
 
         </div>
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
 const Transaction = ({transaction}) => {
+
+    const {deleteTransaction}=useContext(GlobalContext);
+
     const sign=transaction.amount<0?'-':'+';
     return (
         <div>
             <li className={transaction.amount<0?'minus':'plus'}>
-                {transaction.text} <span>{sign} &#x20B9; {Math.abs(transaction.amount)} </span><button className="delete-btn">x</button>
+                {transaction.text} <span>{sign} &#x20B9; {Math.abs(transaction.amount)} </span><button className="delete-btn" onClick={()=>deleteTransaction(transaction.id)}>x</button>
             </li>
         </div>
     )
